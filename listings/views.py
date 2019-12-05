2e1794c869567132b5d3affaab951bfa76c4cf50
evergeenhasib <hasib2407@gmail.com>
2019-12-05 19:23:26 +0600

    Added search fucntionality and created accounts app.

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
+from functools import reduce
+import operator
 
 from .models import Listing
 from listings.choices import bedroom_choices, price_choices, state_choices
@@ -27,10 +30,41 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context )
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords'].split()
+        if keywords:
+            clauses = (Q(description__icontains=keyword) for keyword in keywords)
+            query = reduce(operator.or_, clauses)
+            queryset_list = queryset_list.filter(query)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact = city)
+
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact = state)
+
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte = price)
+
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte = bedrooms)
+
     context ={
         'bedrooms': bedroom_choices,
         'prices' : price_choices,
         'states' : state_choices, 
+        'listings': queryset_list,
+        'values': request.GET,
     }
-    
+
     return render(request, 'listings/search.html', context)
